9ba170a6e3c543df4302a72ac70ea3e7da9dc806
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2025-03-15 12:22:49 -0400

    added dataframes for airport and airport routes

--- airports_data.py
+++ airports_data.py
@@ -3,4 +3,21 @@
 # we will use the pandas library to read the data
 
 import pandas as pd
-import numpy as np
+
+# Airport Data
+airport_columns = ["Airport ID", "Name", "City", "Country", "IATA", "ICAO", "Latitude", "Longitude",
+                   "Altitude", "Timezone", "DST", "Tz database time zone", "Type", "Source"]
+
+airporsts_data = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat"
+
+airports_df = pd.read_csv(airporsts_data, delimiter=",", names=airport_columns)
+print(airports_df.head())
+
+# Airport Routes Data
+airport_columns = ["Airline", "Airline ID", "Source airport", "Source airport ID",
+                   "Destination airport", "Destination airport ID", "Codeshare", "Stops", "Equipment"]
+
+routes_data = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat"
+
+routes_df = pd.read_csv(routes_data, delimiter=",", names=airport_columns)
+print(routes_df.head())
