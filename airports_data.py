01e58dcd78e74563ddc86df46beca2101812c77d
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2025-03-17 23:34:45 -0400

    added data from openflight and handled them in pandas dataframe

--- airports_data.py
+++ airports_data.py
@@ -4,20 +4,36 @@
 
 import pandas as pd
 
-# Airport Data
+"""----------AIRPORT DATA----------"""
 airport_columns = ["Airport ID", "Name", "City", "Country", "IATA", "ICAO", "Latitude", "Longitude",
                    "Altitude", "Timezone", "DST", "Tz database time zone", "Type", "Source"]
 
-airporsts_data = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat"
+airports_data = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat"
 
-airports_df = pd.read_csv(airporsts_data, delimiter=",", names=airport_columns)
-print(airports_df.head())
+airports_df = pd.read_csv(airports_data, delimiter=",", names=airport_columns)
 
-# Airport Routes Data
+airports_df = airports_df.astype(
+    {col: "string" for col in airports_df.select_dtypes(include=["object"]).columns})
+
+# NOTE!!! I kept the city column just in case we need it. There are some rows WITHOUT values for the city column.
+
+"""----------ROUTES DATA----------"""
 airport_columns = ["Airline", "Airline ID", "Source airport", "Source airport ID",
                    "Destination airport", "Destination airport ID", "Codeshare", "Stops", "Equipment"]
 
 routes_data = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat"
 
 routes_df = pd.read_csv(routes_data, delimiter=",", names=airport_columns)
-print(routes_df.head())
+# Columns are not needed
+routes_df = routes_df.drop(["Equipment", "Codeshare"], axis=1)
+
+# replace weird formatting where they used \N to mean NA...
+routes_df = routes_df.replace("\\N", pd.NA)
+
+routes_df.dropna(inplace=True)  # drop all rows with null values
+
+# Change data types for each column respectively
+routes_df = routes_df.astype(
+    {col: "Int64" for col in routes_df if "ID" in col})
+routes_df = routes_df.astype(
+    {col: "string" for col in routes_df.select_dtypes(include=["object"]).columns})
