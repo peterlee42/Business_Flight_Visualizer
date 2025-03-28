845cee015e0680bc790827768684043fde465ead
billy-chen13579 <98634447+billy-chen13579@users.noreply.github.com>
2025-03-28 17:33:15 -0400

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -23,6 +23,9 @@ This file is Copyright (c) 2025 Mario Badr, David Liu, and Isaac Waller.
 import plotly.graph_objects as go
 
 import main
+import dash
+from dash import dcc, html, Output, Input, State
+from geopy.distance import great_circle
 
 import plotly.io as plo
 
@@ -44,47 +47,133 @@ def visualize_graph(graph: main.AirportsGraph, max_vertices: int = 5000, output_
         longitudes.append(lon)
         node_names.append(node)
 
-    edge_lons = []
-    edge_lats = []
+    # edge_lons = []
+    # edge_lats = []
+    edge_traces = []
     for edge in graph_nx.edges:
         node1, node2 = edge
         lat1, lon1 = graph_nx.nodes[node1]['latitude'], graph_nx.nodes[node1]['longitude']
         lat2, lon2 = graph_nx.nodes[node2]['latitude'], graph_nx.nodes[node2]['longitude']
         # None separates the line segments
-        edge_lats.extend([lat1, lat2, None])
-        edge_lons.extend([lon1, lon2, None])
+        # edge_lats.extend([lat1, lat2, None])
+        # edge_lons.extend([lon1, lon2, None])
 
-    fig = go.Figure(go.Scattermap(
-        mode="lines",
-        lon=edge_lons,
-        lat=edge_lats,
-        line={'color': '#11cd2f', 'width': 2},
-        name='Airport Connections',
-        opacity=0.2
-    ))
+        edge_trace = go.Scattermap(
+            mode="lines",
+            lon=[lon1, lon2, None],
+            lat=[lat1, lat2, None],
+            line=dict(width=2, color='rgba(0, 0, 0, 0.1)'),
+            hoverinfo='none' #potentially distance value or cost of living info
+            # name='Airport Connections',
+            #opacity=0.2
+        )
+        edge_traces.append(edge_trace)
 
-    fig.add_trace(go.Scattermap(
+    # edge_traces = go.Scattermap(
+    #     mode="lines",
+    #     lon=edge_lons,
+    #     lat=edge_lats,
+    #     line=dict(width=2, color='rgba(0, 0, 0, 0.1)'),
+    #     name='Airport Connections',
+    #     #opacity=0.2
+    # )
+
+    node_trace = go.Scattermap(
         mode="markers",
         lon=longitudes,
         lat=latitudes,
         text=node_names,
         name='Airports',
-        marker={'size': 6, 'color': 'blue'},
-        opacity=0.6
-    ))
-
+        marker={'size': 6, 'color': 'black'},
+    )
+    fig = go.Figure(data = edge_traces + [node_trace])
     fig.update_layout(
         margin={'l': 0, 't': 0, 'b': 0, 'r': 0},
+        showlegend = False,
         map={
             'center': {'lon': 10, 'lat': 10},
             'style': "open-street-map",
             'center': {'lon': -20, 'lat': -20},
             'zoom': 1,
         },
-        title="Airports Network Visualization"
+        title="Airports Network Visualization",
     )
 
-    if output_file:
-        fig.write_image(output_file)
-    else:
-        fig.show()
+    def change_edge_color(node_name):
+        for i, edge in enumerate(list(graph_nx.edges)):
+            if node_name in edge[0] or node_name in edge[1]:
+                fig.data[i].line.color = 'rgba(0, 0, 0, 0.8)'
+            else:
+                fig.data[i].line.color = 'rgba(0, 0, 0, 0.1)'
+        return fig 
+
+    def change_color_back():
+        for i, edge in enumerate(list(graph_nx.edges)):
+                fig.data[i].line.color = 'rgba(0, 0, 0, 0.1)'
+        return fig 
+
+    # Dash App
+    app = dash.Dash(__name__)
+    app.layout = html.Div([
+        html.H3("Airports Network Visualization"),
+        dcc.Graph(id='world-graph', 
+                  figure=fig,
+                  style={'height': '600px', 'width': '100%'}),
+        html.Div(id='output', style={'marginTop': '20px', 'marginLeft': '300px', 'fontSize': '18px', 'color': 'green'})
+    ])
+    
+    clicked_nodes = []
+
+    @app.callback(
+        Output('output', 'children'),
+        Output('world-graph', 'figure'),
+        Input('world-graph', 'clickData'),
+        prevent_initial_call=True
+    )
+    def display_click(clickData):
+        if not clickData or 'points' not in clickData:
+            return ""
+        
+        point = clickData['points'][0]
+        node_name = point['text']
+
+        if node_name not in graph_nx.nodes:
+            return ""
+
+        # Add clicked node to list
+        clicked_nodes.append(node_name)
+
+        if len(clicked_nodes) < 2:
+            change_edge_color(node_name)
+            return f"Selected node: {node_name}. Click one more node.", fig
+
+        # Compute distance
+        n1, n2 = clicked_nodes[:2]
+        # lat1, lon1 = graph_nx.nodes[n1]['latitude'], graph_nx.nodes[n1]['longitude']
+        # lat2, lon2 = graph_nx.nodes[n2]['latitude'], graph_nx.nodes[n2]['longitude']
+        # pos1 = (lat1, lon1)
+        # pos2 = (lat2, lon2)
+        connected = False
+        for edge in graph_nx.edges(data = True):
+            if n1 in edge and n2 in edge:
+                print(edge[2]["weight"])
+                distance_km = edge[2]["weight"]
+                connected = True
+        if connected:
+            # Reset after two clicks
+            clicked_nodes.clear()
+            change_color_back()
+
+            return f"Distance between {n1} and {n2}: {distance_km:.2f} km", fig
+        else:
+            clicked_nodes.clear()
+            change_color_back()
+            return "Selected node is not connected", fig
+
+
+    app.run(debug=True)
+
+    # if output_file:
+    #     fig.write_image(output_file)
+    # else:
+    #     fig.show()
