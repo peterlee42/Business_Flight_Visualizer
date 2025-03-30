88064bf606bf4d8d9c43f40f072567310e6d08d4
billy-chen13579 <kaichingchen1003@student.kss.tp.edu.tw>
2025-03-29 23:53:30 -0400

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,69 +1,39 @@
-"""CSC111 Winter 2025 Exercise 3-4 (Graphs Visualization)
-
-Module Description
-==================
-
-This module contains some Python functions that you can use to visualize the graphs
-you're working with on this assignment. You should not modify anything in this file.
-It will not be submitted for grading.
-
-Disclaimer: we didn't have time to make this file fully PythonTA-compliant!
-
-Copyright and Usage Information
-===============================
-
-This file is provided solely for the personal and private use of students
-taking CSC111 at the University of Toronto St. George campus. All forms of
-distribution of this code, whether as given or with any changes, are
-expressly prohibited. For more information on copyright for CSC111 materials,
-please consult our Course Syllabus.
-
-This file is Copyright (c) 2025 Mario Badr, David Liu, and Isaac Waller.
-"""
+"""Visualizer for our graph"""
 import plotly.graph_objects as go
-
-import main
-import dash
-from dash import dcc, html, Output, Input, State, ctx
-from geopy.distance import great_circle
-
 import plotly.io as plo
+import dash
+from dash import dcc, html, Output, Input, ctx
+import main
 
 plo.renderers.default = 'browser'
 
 
-def visualize_graph(graph: main.AirportsGraph, max_vertices: int = 5000, output_file: str = ''):
+def visualize_graph(graph: main.AirportsGraph, max_vertices: int = 5000) -> None:
     """Visualize graph on map"""
     graph_nx = graph.to_networkx(max_vertices)
-
-    # latitudes = []
-    # longitudes = []
-    # node_names = []
 
     node_traces = []
     for node in graph_nx.nodes:
         lat1 = graph_nx.nodes[node]['latitude']
         lon1 = graph_nx.nodes[node]['longitude']
         id1 = graph_nx.nodes[node]['id']
-        # latitudes.append(lat1)
-        # longitudes.append(lon1)
-        # node_names.append(node)
+
         node_trace = go.Scattermap(
             mode="markers",
-            lon = [lon1],
-            lat = [lat1],
-            text = [node],
-            name = "Airports",
+            lon=[lon1],
+            lat=[lat1],
+            text=[node],
+            name="Airports",
             marker={'size': 4, 'color': 'black'},
-            ids = [id1]
+            ids=[id1]
         )
         node_traces.append(node_trace)
 
     edge_traces = []
     text_traces = []
     for edge in graph_nx.edges(data=True):
-        node1= edge[0]
-        node2= edge[1]
+        node1 = edge[0]
+        node2 = edge[1]
         lat1, lon1 = graph_nx.nodes[node1]['latitude'], graph_nx.nodes[node1]['longitude']
         lat2, lon2 = graph_nx.nodes[node2]['latitude'], graph_nx.nodes[node2]['longitude']
 
@@ -71,7 +41,7 @@ def visualize_graph(graph: main.AirportsGraph, max_vertices: int = 5000, output_
             mode="lines+text",
             lon=[lon1, lon2, None],
             lat=[lat1, lat2, None],
-            line=dict(width=2, color='rgba(0, 0, 0, 0.1)'),
+            line={"width": 2, "color": 'rgba(0, 0, 0, 0.1)'},
         )
         edge_traces.append(edge_trace)
 
@@ -87,47 +57,35 @@ def visualize_graph(graph: main.AirportsGraph, max_vertices: int = 5000, output_
             text=[str(edge[2]['weight']) + 'km'],
             textposition="middle center",
             showlegend=False,
-            textfont=dict(size=8)
+            textfont={"size": 8}
         )
         text_traces.append(text_trace)
 
-    fig = go.Figure(data = edge_traces + node_traces + text_traces)
+    fig = go.Figure(data=edge_traces + node_traces + text_traces)
     fig.update_layout(
         margin={'l': 0, 't': 0, 'b': 0, 'r': 0},
-        showlegend = False,
+        showlegend=False,
         map={
             'center': {'lon': 10, 'lat': 10},
             'style': "open-street-map",
-            'center': {'lon': -20, 'lat': -20},
             'zoom': 1,
         },
         title="Airports Network Visualization",
     )
 
-    def change_edge_color(node_name):
-        for i, edge in enumerate(list(graph_nx.edges)):
-            if node_name in edge[0] or node_name in edge[1]:
-                fig.data[i].line.color = 'rgba(0, 0, 0, 0.8)'
-            else:
-                fig.data[i].line.color = 'rgba(0, 0, 0, 0.1)'
-        return fig 
-
-    def change_color_back():
-        for i, edge in enumerate(list(graph_nx.edges)):
-                fig.data[i].line.color = 'rgba(0, 0, 0, 0.1)'
-        return fig 
-    
-    def change_node_color(node_name):
+    def change_node_color(node_name: str) -> go.Figure:
+        """docstring"""
         for i, value in enumerate(fig.data):
             if value in edge_traces or value in text_traces:
                 pass
             else:
                 if node_name == fig.data[i].text[0]:
                     fig.data[i].marker = {'size': 10, 'color': 'black'}
-                    #print(fig.data[i])
                     return fig
-    
-    def change_node_back():
+        return fig
+
+    def change_node_back() -> go.Figure:
+        """docstring"""
         for i, value in enumerate(fig.data):
             if value in edge_traces or value in text_traces:
                 pass
@@ -135,23 +93,9 @@ def visualize_graph(graph: main.AirportsGraph, max_vertices: int = 5000, output_
                 fig.data[i].marker = {'size': 4, 'color': 'black'}
         return fig
 
-
     # Dash App
     app = dash.Dash(__name__)
-    # app.layout = html.Div([
-    #     html.H3("Business travel flight visualizer"),
-    #     dcc.Graph(id='world-graph', 
-    #               figure=fig,
-    #               style={'height': '600px', 'width': '100%'}),
-    #     html.Div(
-    #         ["Max Distance: ", dcc.Input(id='my-input', value='1000', type='text')], 
-    #         style={'marginLeft': '40%','marginTop': '2%'}),
-    #     html.Div(
-    #         "Output: ",
-    #         style = {'marginLeft':'30%','marginTop':'0.2%'}),
-    #     html.Button(id='submit-button-state', children='Submit',style={'marginLeft':'60%'}),
-    #     html.Div(id='output', style={'marginTop': '0.5%', 'fontSize': '14px', 'color': 'green'})
-    # ])
+
     app.layout = html.Div(
         style={
             'backgroundColor': '#f9f9f9',
@@ -229,7 +173,7 @@ def visualize_graph(graph: main.AirportsGraph, max_vertices: int = 5000, output_
                 }
             )
         ]
-        )
+    )
 
     clicked_nodes_name = []
     clicked_node = []
@@ -242,77 +186,59 @@ def visualize_graph(graph: main.AirportsGraph, max_vertices: int = 5000, output_
         Input('submit-button-state', 'n_clicks'),
         prevent_initial_call=True
     )
-    def display_click(clickData, max_distance, button_state):
-        # print(clickData, max_distance)
-        # print(ctx.triggered_id)
-        result = ""
+    def display_click(clickdata: any, max_distance: any, button_state: any) -> (str, go.Figure):
+        """docstring"""
         if ctx.triggered_id == 'submit-button-state':
             if len(clicked_node) == 0:
                 return 'please lick one airport'
-            
+
             id_list = []
+            result1 = ""
             for i in clicked_node:
                 id_list.append(i['points'][0]['id'])
                 result1 = main.AirportsGraph.get_close_airports(graph, id_list, int(max_distance))
             res = ', '.join(result1)
             change_node_back()
-            #change_color_back()
             clicked_nodes_name.clear()
             clicked_node.clear()
             return f'The intersection airports include: {res}', fig
-        
+
         elif ctx.triggered_id == 'world-graph':
-            #print(clickData)
-            if not clickData or 'points' not in clickData:
+            if not clickdata or 'points' not in clickdata:
                 return ""
-            
-            point = clickData['points'][0]
+
+            point = clickdata['points'][0]
             node_name = point['text']
 
             if node_name not in graph_nx.nodes:
                 return ""
-            
-            # if len(clicked_node) < 2:
-            #     change_edge_color(node_name)
 
             # Add clicked node to list
             clicked_nodes_name.append(node_name)
-            clicked_node.append(clickData)
+            clicked_node.append(clickdata)
             change_node_color(node_name)
             result = ', '.join(clicked_nodes_name)
 
-            return f'Selected node: {result}',fig
-        
-        elif ctx.triggered_id == 'my-input':
             return f'Selected node: {result}', fig
 
-        # # Compute distance
-        # n1, n2 = clicked_nodes[:2]
-        # # lat1, lon1 = graph_nx.nodes[n1]['latitude'], graph_nx.nodes[n1]['longitude']
-        # # lat2, lon2 = graph_nx.nodes[n2]['latitude'], graph_nx.nodes[n2]['longitude']
-        # # pos1 = (lat1, lon1)
-        # # pos2 = (lat2, lon2)
+        elif ctx.triggered_id == 'my-input':
+            result = ', '.join(clicked_nodes_name)
+            return f'Selected node: {result}', fig
 
-        # connected = False
-        # for edge in graph_nx.edges(data = True):
-        #     if n1 in edge and n2 in edge:
-        #         distance_km = edge[2]["weight"]
-        #         connected = True
-        # if connected:
-        #     # Reset after two clicks
-        #     clicked_nodes.clear()
-        #     change_color_back()
-
-        #     return f"Distance between {n1} and {n2}: {distance_km:.2f} km", fig
-        # else:
-        #     clicked_nodes.clear()
-        #     change_color_back()
-        #     return "Selected node is not directly connected", fig
-
+        return "", fig
 
     app.run(debug=True)
 
-    # if output_file:
-    #     fig.write_image(output_file)
-    # else:
-    #     fig.show()
+
+# if __name__ == "__main__":
+#     import doctest
+#
+#     doctest.testmod()
+#
+#     import python_ta
+#
+#     python_ta.check_all(config={
+#         'extra-imports': [],  # the names (strs) of imported modules
+#         'allowed-io': [],  # the names (strs) of functions that call print/open/input
+#         'max-line-length': 120
+#     })
